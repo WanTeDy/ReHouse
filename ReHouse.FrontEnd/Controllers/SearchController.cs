2b46e005501ba3641cb0c0b7cf5be0d76c8335e7
Wantedy <wanted.kaiser228@gmail.com>
2017-06-17 20:38:11 +0300

    serach added

---
+++ ReHouse.FrontEnd/Controllers/SearchController.cs
@@ -0,0 +1,64 @@
+﻿#define CASH
+
+using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+using ReHouse.Utils.BusinessOperations.Cart;
+using ReHouse.Utils.DataBase;
+using ReHouse.Utils.BusinessOperations.Home;
+using ReHouse.Utils.Helpers;
+using ReHouse.FrontEnd.Helpers;
+using ReHouse.FrontEnd.Models;
+using ReHouse.Utils.BusinessOperations.Common;
+using ReHouse.Utils;
+
+namespace ReHouse.FrontEnd.Controllers
+{
+    public class SearchController : BaseController
+    {
+        public ActionResult Flats(string query)
+        {
+            ViewBag.NoElements = true;
+            var sessionModel = SessionHelpers.Session("user", typeof(SessionModel)) as SessionModel;
+            var tokenHash = "";
+            if (sessionModel != null)
+                tokenHash = sessionModel.TokenHash;
+
+            if (!String.IsNullOrEmpty(query))
+            {
+                var operation = new SearchAdvertsOperation(tokenHash, query, 1, ConstV.ItemsPerPage);
+                operation.ExcecuteTransaction();
+
+                if (operation._adverts != null && operation._adverts.Count > 0)
+                    ViewBag.NoElements = false;
+                var model = new LoadCartModel()
+                {
+                    Adverts = operation._adverts,
+                };
+                return View(model);
+            }
+            return View();
+        }
+
+        //[HttpPost]
+        //public JsonResult Load(CartModel model)
+        //{
+        //    var cart = SessionHelpers.Session("Cart") as List<CartModel> ?? new List<CartModel>();
+        //    var adv = cart.Find(x => x.AdvertId == model.AdvertId && x.Type == model.Type);
+        //    if (adv == null && model.IsAdd)
+        //    {
+        //        cart.Add(model);
+        //    }
+        //    else if (adv != null && !model.IsAdd)
+        //    {
+        //        cart.Remove(adv);
+        //    }
+        //    SessionHelpers.Session("Cart", cart);
+        //    return Json(new { NoError = true });
+        //}
+    }
+}
---
+++ ReHouse.Utils/BusinessOperations/Common/SearchAdvertsOperation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ReHouse.Utils.Helpers;
+using ReHouse.Utils.DataBase.AdvertParams;
+using ReHouse.Utils.DataBase.News;
+
+namespace ReHouse.Utils.BusinessOperations.Common
+{
+    public class SearchAdvertsOperation : BaseOperation
+    {
+        private String _tokenHash { get; set; }
+        private String _searchRequest { get; set; }
+        private Int32 _page { get; set; }
+        private Int32 _count { get; set; }
+        public List<CartAdvertModel> _adverts { get; set; }
+        //public List<Article> _articles { get; set; }
+
+        public SearchAdvertsOperation(string tokenHash, string searchRequest, int page, int count)
+        {
+            _tokenHash = tokenHash;
+            _searchRequest = searchRequest;
+            _page = page;
+            _count = count;
+            RussianName = "Получение объявлений по поиску";
+        }
+
+        protected override void InTransaction()
+        {
+            //var check = new CheckUserRoleAuthorityOperation(_tokenHash, Name, RussianName);
+
+            var str = _searchRequest.ToLower().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            var flats = Context.Adverts.Where(x => !x.Deleted && x.IsModerated && str.All(r => x.Title.RussianName.ToLower().Contains(r) || x.Description.ToLower().Contains(r) || x.District.RussianName.ToLower().Contains(r) || x.AdvertPropertyValues.All(y => y.PropertiesValue.Contains(r))))
+                .OrderByDescending(x => x.PublicationDate).Skip((_page - 1) * _count).Take(_count).ToList();
+            _adverts = new List<CartAdvertModel>();
+            _adverts.AddRange(flats.Select(x =>
+
+                new CartAdvertModel()
+                {
+                    Id = x.Id,
+                    Price = x.Price,
+                    Adress = x.Street,
+                    Type = x.Type,
+                    Description = x.Description.Length > ConstV.DescMinimizeSymbols + 5 ? x.Description.Substring(0, ConstV.DescMinimizeSymbols) + "..." : x.Description,
+                    Name = x.Title.RussianName,
+                    //Name = el.TitleName,
+                    Image = x.Images.FirstOrDefault(y => !x.Deleted),
+                    IsHot = x.IsHot,
+                    IsExclusive = x.IsExclusive,
+                }
+
+            ));
+
+            var newBuilding = Context.NewBuildings.Where(x => !x.Deleted && x.IsModerated && str.All(r => x.Name.ToLower().Contains(r) || x.Description.ToLower().Contains(r) || x.Adress.ToLower().Contains(r) || x.Construct.ToLower().Contains(r)
+                || x.District.RussianName.ToLower().Contains(r) || x.Heating.ToLower().Contains(r) || x.Parking.ToLower().Contains(r)))
+                .OrderByDescending(x => x.IsHot).ThenByDescending(x => x.PublicationDate).Take(_count).ToList();
+
+            _adverts.AddRange(newBuilding.Select(x =>
+
+                new CartAdvertModel()
+                {
+                    Id = x.Id,
+                    Price = x.Price,
+                    Adress = x.Adress,
+                    Type = AdvertsType.NewBuilding,
+                    Description = x.ExpluatationDate.Name,
+                    Name = x.Name,
+                    //Name = el.TitleName,
+                    Image = x.Images.FirstOrDefault(y => !x.Deleted),
+                    IsHot = x.IsHot,
+                    IsExclusive = x.IsExclusive,
+                }
+
+            ));
+            
+            //_articles = Context.Articles.Where(x => !x.Deleted)
+            //    .OrderByDescending(x => x.Date).Take(_articlesCount).ToList();
+        }
+    }
+}
