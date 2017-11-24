1002776daf6be695dc3d21fe0592c594f3677d3f
Sergey <Sergey@DESKTOP-0EAVFJH>
2017-11-24 11:53:16 +0200

    updates

--- ReHouse.Utils/DataBase/Common/SliderParam.cs
+++ ReHouse.Utils/DataBase/Common/SliderParam.cs
@@ -12,7 +12,12 @@ namespace ReHouse.Utils.DataBase.Common
         /// Url
         /// </summary>       
         public String Url { get; set; }
-        
+
+        /// <summary>
+        /// Url
+        /// </summary>       
+        public String ButtonUrl { get; set; }
+
         /// <summary>
         /// is this url video
         /// </summary>       
